0774482c47fa268c539c06db297842c8717cbf44
fcgoesdev <fcgoesdev@gmail.com>
2023-11-21 19:09:47 -0300

    Segundo commit!

--- projeto_game_01.py
+++ projeto_game_01.py
@@ -21,6 +21,31 @@ while True:
             print('******************************************\n')
     else:
         print('\nInválido, tente de novo !\n')
-print('\n******************************************')
-print(f'O número de jogadores será: {jogadores}.')
-print('******************************************\n')
+
+max_score = 50
+player_score = [0 for _ in range(jogadores)]
+
+while max(player_score) < max_score:
+
+    
+    for player_idx in range(jogadores):
+        print('\nJogador Número', player_idx + 1 'sua rodada iniciou!')
+        print('Seu placar é:', player_score[player_idx],'\n'])
+        current_score = 0
+
+        while True:
+            should_roll = input('Você gostaria de jogar o dado (s) ?')
+
+            if should_roll.lower() != "s":
+                break
+
+            value = roll()
+            if value == 1:
+                print('você tirou 1, seu score zerou!')
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print('Você tirou:', value)
+            
+            print('Seu placar é: ', current_score)
